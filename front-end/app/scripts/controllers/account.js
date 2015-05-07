eda1acf2b657322a483597137ff57c90e211e6f6
Tim Hodson <tim@timhodson.com>
2015-05-07 13:13:32 +0100

    adding profile stuff

--- front-end/app/scripts/controllers/account.js
+++ front-end/app/scripts/controllers/account.js
@@ -9,10 +9,40 @@
 angular.module('disturbometerApp')
   .controller('AccountCtrl', function ($scope, user, Auth, Ref, $firebaseObject) {
     $scope.user = user;
-    $scope.logout = function() { Auth.$unauth(); };
-    $scope.messages = [];
-    var profile = $firebaseObject(Ref.child('users/'+user.uid));
-    profile.$bindTo($scope, 'profile');
-    
+    $scope.logout = function () {
+      Auth.$unauth();
+    };
+    $scope.profile = $firebaseObject(Ref.child('users/' + user.uid));
 
+    /**
+     * Sync google profile with our firebase profile.
+     */
+
+    // sync the logged in user with their account profile
+    $scope.profile.givenName = $scope.user.google.cachedUserProfile.given_name || null;
+    $scope.profile.picture = $scope.user.google.cachedUserProfile.picture || null;
+    $scope.profile.displayName = $scope.user.google.displayName || null;
+    $scope.profile.email = $scope.user.google.email || null;
+    $scope.profile.macAddresses = $scope.profile.macAddresses  || null;
+
+    /**
+     * Mac address management
+     */
+    $scope.newMacAddressName = '';
+    $scope.newMacAddress = '';
+
+    $scope.addMacAddress = function () {
+      if (typeof $scope.profile.macAddresses === 'undefined') {
+        $scope.profile.macAddresses = {};
+      }
+      $scope.profile.macAddresses[$scope.newMacAddressName] = $scope.newMacAddress;
+      $scope.newMacAddressName = '';
+      $scope.newMacAddress = '';
+      $scope.profile.$save();
+    };
+
+    $scope.removeMacAddress = function (macKey) {
+      delete $scope.profile.macAddresses[macKey];
+      $scope.profile.$save();
+    };
   });
