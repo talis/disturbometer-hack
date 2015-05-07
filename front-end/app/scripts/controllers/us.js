2a6ba314340a25ccfa05b3a42ecd5afb3f6015e8
russellhill <russell.hill@gmail.com>
2015-05-07 11:15:25 +0100

    add user directive and images etc

--- front-end/app/scripts/controllers/us.js
+++ front-end/app/scripts/controllers/us.js
@@ -7,7 +7,59 @@
  * A demo of using AngularFire to manage a synchronized list.
  */
 angular.module('disturbometerApp')
-  .controller('UsCtrl', function ($scope, Ref, $firebaseArray, $timeout) {
+  .controller('UsCtrl', function ($scope) {
+//  .controller('UsCtrl', function ($scope, Ref, $firebaseArray, $timeout) {
+      var users = [
+            {   'name'  : 'Russell Hill',
+                'email' : 'rh@talis.com',
+                'status': 'available',
+                'inOffice' : true},  
+            {   'name'  : 'Tim Hodson',
+                'email' : 'tim.hodson@talis.com',
+                'status': 'unavailable',
+                'inOffice' : true},  
+            {   'name'  : 'Jeremy Baines',
+                'email' : 'jeremy.baines@talis.com',
+                'status': 'disturbable',
+                'inOffice' : true},  
+            {   'name'  : 'Mark Wallsgrove',
+                'email' : 'mw@talis.com',
+                'status': 'disturbable',
+                'inOffice' : true},  
+            {   'name'  : 'Matt Moran',
+                'email' : 'mm@talis.com',
+                'status': 'disturbable',
+                'inOffice' : true},  
+            {   'name'  : 'Arunn Ramadoss',
+                'email' : 'ar@talis.com',
+                'status': 'disturbable',
+                'inOffice' : true},  
+            {   'name'  : 'Nigel Ashworth',
+                'email' : 'nigel.ashworth@talis.com',
+                'status': 'disturbable',
+                'inOffice' : true},  
+            {   'name'  : 'Nadeem Shabir',
+                'email' : 'ns@talis.com',
+                'status': 'disturbable',
+                'inOffice' : true},  
+            {   'name'  : 'Richard Gubby',
+                'email' : 'rg@talis.com',
+                'status': 'disturbable',
+                'inOffice' : true},  
+            {   'name'  : 'Richard Tattersall',
+                'email' : 'rt@talis.com',
+                'status': 'disturbable',
+                'inOffice' : true},  
+            {   'name'  : 'Chris Clarke',
+                'email' : 'cc@talis.com',
+                'status': 'disturbable',
+                'inOffice' : true}  
+      ];
+      
+      console.log(users);
+      
+      $scope.users = users;
+      
     // synchronize a read-only, synchronized array of messages, limit to most recent 10
 //    $scope.messages = $firebaseArray(Ref.child('messages').limitToLast(10));
 
@@ -32,5 +84,5 @@ angular.module('disturbometerApp')
 //    }
 
 
-
+console.log('here!!!');
   });
